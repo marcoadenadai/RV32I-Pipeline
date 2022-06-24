7485388faccfc5ff818d1c076d92be67d15f50ba
alcidesbeato <71664777+alcidesbeato@users.noreply.github.com>
2022-06-24 01:50:04 -0300

    add test bench ula and registers
    Co-Authored-By: derekfq <50533501+derekfq@users.noreply.github.com>
    Co-Authored-By: Rafael Silva Barbon <67515044+RafaelBarbon@users.noreply.github.com>
    Co-Authored-By: marcoadenadai <32654919+marcoadenadai@users.noreply.github.com>

---
+++ TB_Banco_Registradores.vhd
@@ -0,0 +1,86 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL; -- tipos std_logic e std_logic_vector
+use IEEE.numeric_std.ALL; -- tipos unsigned e signed
+library work;
+USE work.BancoRegistradores_PACKAGE.all;
+use std.textio.ALL; -- deve ser incluido para uso de arquivos
+
+ 
+entity TB_Banco_Registradores is
+end TB_Banco_Registradores;
+
+architecture TB_Banco_Registradores_ARCH of TB_Banco_Registradores is
+	constant PERIODO : time := 10 ns;
+	signal RegWrite : STD_LOGIC; 	--input
+	signal W_CLK : STD_LOGIC := '0'; 	--input
+	signal Reset : STD_LOGIC; 	--input
+
+	signal ReadRegister1 : STD_LOGIC_VECTOR(4 DOWNTO 0); 	--input
+	signal ReadRegister2 : STD_LOGIC_VECTOR(4 DOWNTO 0); 	--input
+	signal WriteRegister : STD_LOGIC_VECTOR(4 DOWNTO 0); 	--input
+
+	signal WriteData : STD_LOGIC_VECTOR(31 DOWNTO 0); 	--input
+	signal ReadData1 : STD_LOGIC_VECTOR(31 DOWNTO 0); 	--output
+	signal ReadData2 : STD_LOGIC_VECTOR(31 DOWNTO 0); 	--output
+
+
+begin
+
+	W_CLK <= not W_CLK after PERIODO/2; 
+	
+	BLOCO : BancoRegistradores PORT MAP(	
+		RegWrite,
+		W_CLK,
+		Reset,		
+		ReadRegister1, 
+		ReadRegister2,
+		WriteRegister,
+		WriteData,
+		ReadData1,
+		ReadData2
+	);
+
+	tb1 : process
+			constant period: time := 100 ns;
+			
+			begin
+
+			-- First Read
+			Reset <= '1';
+			wait for period;
+			Reset <= '0';
+			wait for period;
+			RegWrite <= '0';
+			ReadRegister1 <= "00000"; -- endereço registrador vai ser lido
+			ReadRegister2 <= "00001"; -- endereço registrador vai ser lido
+			wait for period;
+			assert ( (ReadData1 <= "00000000000000000000000000000000" AND ReadData2 = "00000000000000000000000000000000") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no Banco de registradores (1)" severity error;
+
+			wait for period;
+			-- Write
+			RegWrite <= '1';
+			ReadRegister2 <= "00001"; -- endereço registrador vai ser lido
+			WriteRegister <= "00001"; -- endereço do registrador que vai ser escrito
+			WriteData <= "11111111111111111111111111111111";
+			wait for period;
+			assert ( (ReadData2 = "11111111111111111111111111111111") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no Banco de registradores (2)" severity error;
+
+			-- Teste
+			-- Read
+			RegWrite <= '0';
+			ReadRegister1 <= "00000"; -- endereço registrador vai ser lido
+			ReadRegister2 <= "00001"; -- endereço registrador vai ser lido
+			wait for period;
+			assert ( (ReadData1 <= "00000000000000000000000000000000" AND ReadData2 = "11111111111111111111111111111111") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no Banco de registradores (3)" severity error;
+			
+
+			wait;
+		end process;
+
+end architecture;
--- TB_ULA.vhd
+++ TB_ULA.vhd
@@ -28,13 +28,12 @@ begin
 											W_A,
 											W_B,
 											W_OUT);
-	--
-	
+
 	tb1 : process
 			constant period: time := 100 ns;
 			
 			begin
-			
+			-- AND -------------------------------------------------------------------
 			W_ULA_OP <= "0000";
 			W_A <= "11111111111111111111111111111111";
 			W_B <= "00000000000000000000000000000001";
@@ -50,6 +49,288 @@ begin
 			assert ( (W_OUT = "00000000000000000000000000010001") ) -- expected output
 			-- error will be reported if output != expected
 			report "ERRO no AND (2)" severity error;
+
+			W_ULA_OP <= "0000";
+			W_A <= "10101010101010101010101010101010";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "00000000000000000000000000000000") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no AND (3)" severity error;
+
+			W_ULA_OP <= "0000";
+			W_A <= "01010101010101010101010101010101";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "00000000000000000000000000010001") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no AND (4)" severity error;
+
+			-- OR -------------------------------------------------------------------
+			W_ULA_OP <= "0001";
+			W_A <= "11111111111111111111111111110000";
+			W_B <= "00000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "11111111111111111111111111110001") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no OR (1)" severity error;
+			
+			W_ULA_OP <= "0001";
+			W_A <= "11111111111111111111111100000000";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "11111111111111111111111100010001") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no OR (2)" severity error;
+
+			W_ULA_OP <= "0001";
+			W_A <= "10101010101010101010101010101010";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "10101010101010101010101010111011") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no OR (3)" severity error;
+
+			W_ULA_OP <= "0001";
+			W_A <= "01010101010101010101010101010101";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "01010101010101010101010101010101") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no OR (4)" severity error;
+
+
+			-- SUM -------------------------------------------------------------------
+			W_ULA_OP <= "0010";
+			W_A <= "00001111111111111111111100000000";
+			W_B <= "01000000000000000000000000000000";
+			wait for period;
+			assert ( (W_OUT = "01001111111111111111111100000000") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SUM (1)" severity error;
+			
+			W_ULA_OP <= "0010";
+			W_A <= "11111111111111111111111111110000";
+			W_B <= "00000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "11111111111111111111111111110001") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SUM (2)" severity error;
+
+			W_ULA_OP <= "0010";
+			W_A <= "10101010101010101010101010101010";
+			W_B <= "01000000000000000000000000000000";
+			wait for period;
+			assert ( (W_OUT = "11101010101010101010101010101010") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SUM (3)" severity error;
+			
+			W_ULA_OP <= "0010";
+			W_A <= "01010101010101010101010101010101";
+			W_B <= "00000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "01010101010101010101010101010110") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SUM (4)" severity error;
+
+			-- SUB -------------------------------------------------------------------
+			W_ULA_OP <= "0011";
+			W_A <= "11111111111111111111111111111111";
+			W_B <= "10000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "01111111111111111111111111111110") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SUB (1)" severity error;
+			
+			W_ULA_OP <= "0011";
+			W_A <= "11111111111111111111111111111111";
+			W_B <= "11000000000000000000000000000000";
+			wait for period;
+			assert ( (W_OUT = "00111111111111111111111111111111") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SUB (2)" severity error;
+
+			W_ULA_OP <= "0011";
+			W_A <= "10101010101010101010101010101010";
+			W_B <= "10000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "00101010101010101010101010101001") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SUB (3)" severity error;
+			
+			W_ULA_OP <= "0011";
+			W_A <= "01010101010101010101010101010101";
+			W_B <= "11000000000000000000000000000000";
+			wait for period;
+			assert ( (W_OUT = "10010101010101010101010101010101") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SUB (4)" severity error;
+
+			-- XOR -------------------------------------------------------------------
+			W_ULA_OP <= "0100";
+			W_A <= "11111111111111111111111111111111";
+			W_B <= "00000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "11111111111111111111111111111110") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no XOR (1)" severity error;
+			
+			W_ULA_OP <= "0100";
+			W_A <= "11111111111111111111111111111111";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "11111111111111111111111111101110") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no XOR (2)" severity error;
+
+			W_ULA_OP <= "0100";
+			W_A <= "10101010101010101010101010101010";
+			W_B <= "00000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "10101010101010101010101010101011") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no XOR (3)" severity error;
+			
+			W_ULA_OP <= "0100";
+			W_A <= "01010101010101010101010101010101";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "01010101010101010101010101000100") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no XOR (4)" severity error;
+
+			-- SHIFT LEFT ARITMETICO -------------------------------------------------------------------
+			W_ULA_OP <= "0101";
+			W_A <= "11111111111111111111111111111111";
+			W_B <= "00000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "11111111111111111111111111111110") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT LEFT ARITMETICO (1)" severity error;
+			
+			W_ULA_OP <= "0101";
+			W_A <= "11111111111111111111111111111111";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "11111111111111100000000000000000") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT LEFT ARITMETICO (2)" severity error;
+
+			W_ULA_OP <= "0101";
+			W_A <= "10101010101010101010101010101010";
+			W_B <= "00000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "01010101010101010101010101010100") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT LEFT ARITMETICO (3)" severity error;
+			
+			W_ULA_OP <= "0101";
+			W_A <= "01010101010101010101010101010101";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "10101010101010100000000000000000") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT LEFT ARITMETICO (4)" severity error;
+
+			-- SHIFT RIGHT ARITMETICO -------------------------------------------------------------------
+			W_ULA_OP <= "0110";
+			W_A <= "11111111111111111111111111111111";
+			W_B <= "00000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "11111111111111111111111111111111") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT RIGHT ARITMETICO (1)" severity error;
+			
+			W_ULA_OP <= "0110";
+			W_A <= "11111111111111111111111111111111";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "11111111111111111111111111111111") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT RIGHT ARITMETICO (2)" severity error;
+
+			W_ULA_OP <= "0110";
+			W_A <= "10101010101010101010101010101010";
+			W_B <= "00000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "11010101010101010101010101010101") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT RIGHT ARITMETICO (3)" severity error;
+			
+			W_ULA_OP <= "0110";
+			W_A <= "01010101010101010101010101010101";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "00000000000000000010101010101010") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT RIGHT ARITMETICO (4)" severity error;
+
+			-- SHIFT LEFT LOGICO -------------------------------------------------------------------
+			W_ULA_OP <= "0111";
+			W_A <= "11111111111111111111111111111111";
+			W_B <= "00000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "11111111111111111111111111111110") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT LEFT LOGICO (1)" severity error;
+			
+			W_ULA_OP <= "0111";
+			W_A <= "11111111111111111111111111111111";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "11111111111111100000000000000000") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT LEFT LOGICO (2)" severity error;
+
+			W_ULA_OP <= "0111";
+			W_A <= "10101010101010101010101010101010";
+			W_B <= "00000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "01010101010101010101010101010100") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT LEFT LOGICO (3)" severity error;
+			
+			W_ULA_OP <= "0111";
+			W_A <= "01010101010101010101010101010101";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "10101010101010100000000000000000") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT LEFT LOGICO (4)" severity error;
+
+			-- SHIFT RIGHT LOGICO -------------------------------------------------------------------
+
+			W_ULA_OP <= "1000";
+			W_A <= "11111111111111111111111111111111";
+			W_B <= "00000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "01111111111111111111111111111111") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT RIGHT LOGICO (1)" severity error;
+			
+			W_ULA_OP <= "1000";
+			W_A <= "11111111111111111111111111111110";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "00000000000000000111111111111111") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT RIGHT LOGICO (2)" severity error;
+
+			W_ULA_OP <= "1000";
+			W_A <= "10101010101010101010101010101010";
+			W_B <= "00000000000000000000000000000001";
+			wait for period;
+			assert ( (W_OUT = "01010101010101010101010101010101") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT RIGHT LOGICO (3)" severity error;
+			
+			W_ULA_OP <= "1000";
+			W_A <= "01010101010101010101010101010101";
+			W_B <= "00000000000000000000000000010001";
+			wait for period;
+			assert ( (W_OUT = "00000000000000000010101010101010") ) -- expected output
+			-- error will be reported if output != expected
+			report "ERRO no SHIFT RIGHT LOGICO (4)" severity error;
 			
 
 			wait;
